da71d26b452288cbb4bc8a051ac09657bba1be7b
bsparkes <bsparkes@stanford.edu>
2018-06-04 00:12:44 -0700

    Started typing up the paper, turns out there were more subtle differences between the original experiment and replication than I'd thought…

--- paper/auto/writeup.el
+++ paper/auto/writeup.el
@@ -4,13 +4,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("babel" "english") ("fontenc" "T1") ("mtpro2" "complete" "subscriptcorrection" "slantedGreek" "mtpfrak" "mtpbbi" "mtpcal") ("fontspec" "no-math") ("biblatex" "backend=biber" "style=authoryear-comp" "citestyle=authoryear-comp" "backref=false" "hyperref=true" "url=false" "isbn=false" "") ("enumitem" "inline") ("hyperref" "hidelinks" "breaklinks")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("inputenc" "utf8") ("babel" "english") ("geometry" "margin=1in") ("fontenc" "T1") ("mtpro2" "complete" "subscriptcorrection" "slantedGreek" "mtpfrak" "mtpbbi" "mtpcal") ("fontspec" "no-math") ("biblatex" "backend=biber" "style=authoryear-comp" "citestyle=authoryear-comp" "backref=false" "hyperref=true" "url=false" "isbn=false" "") ("enumitem" "inline") ("hyperref" "hidelinks" "breaklinks")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -18,6 +18,7 @@
     "art10"
     "inputenc"
     "babel"
+    "geometry"
     "fontenc"
     "mtpro2"
     "bm"
@@ -44,8 +45,12 @@
    (TeX-add-symbols
     '("sem" 1))
    (LaTeX-add-labels
+    "sec:things-note"
     "sec:introduction"
-    "sec:things-note")
+    "sec:experiment"
+    "sec:method"
+    "sec:results"
+    "sec:discussion")
    (LaTeX-add-bibliographies
     "ling245"))
  :latex)
