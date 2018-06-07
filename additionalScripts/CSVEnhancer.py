2896d37983514fca17b8ad7bd2b387caf7d25bf3
bsparkes <bsparkes@stanford.edu>
2018-06-07 15:50:03 -0700

    Small script to add labels for the position of the trail for each participant, which was implicit (via order) but not explicit. Updated CSV created, and scripts added to examine difference between first and second half of the experiment.

---
+++ additionalScripts/CSVEnhancer.py
@@ -0,0 +1,37 @@
+import csv
+import re
+
+
+with open('../data/bottchemla2016.csv', encoding='utf-8') as f:
+  reader = list(csv.DictReader(f))
+
+  currentWorkerID = reader[0]['workerid']
+  trialNumber = 1
+
+  for q in reader:
+
+    if q['workerid'] == currentWorkerID:
+      pass
+    else:
+      currentWorkerID = q['workerid']
+      trialNumber = 1
+
+    if q['trial_type'] == '"response"':
+      q['trialNumber'] = trialNumber
+      trialNumber += 1
+    else:
+      q['trialNumber'] = 0
+
+keys = reader[0].keys()
+with open('../data/bottchemla2016Numbered.csv', 'w', newline='',encoding='utf-8') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(reader)
+
+    # print(q['workerid'])
+  # for q in reader:
+    # q['Question'] = re.sub(r'Prime(\s+Minister){0,1}', r'$person', q['Question'])
+    # q['Question'] = re.sub(r'(right\s+){0,1}hon\.(\s+(?:Friend|Member|Gentleman|Lady)){0,1}', r'$person', q['Question'])
+    # q['Question'] = re.sub(r'(right\s+){0,1}[Hh]on\.\s+(?:(?:Opposition\s+){0,1}Members)', r'$people', q['Question'])
+    # q['Question'] = re.sub(r'\s(?:[Hh]e|[Ss]he|[Hh]is|[Hh]er)\s', r' $pronoun ', q['Question'])
+    # print(q)
